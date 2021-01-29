9e46cb5962f65a4757736302e3448646b48a54eb
maycooliveira <maycoasamoda@hotmail.com>
2021-01-29 12:49:42 -0300

    fix server eslint

--- src/services/api.js
+++ src/services/api.js
@@ -1,38 +1,32 @@
-/* eslint-disable */
 import AsyncStorage from '@react-native-async-storage/async-storage';
 import axios from 'axios';
 import app from '../configs/app';
 const accessToken = 'access-token';
 
 function instance() {
-  let index;
-
   let axiosInstance = axios.create({
     baseURL: app.URL_BASE,
   });
 
-  axiosInstance.interceptors.request.use(async request => {
+  axiosInstance.interceptors.request.use(async (request) => {
     try {
-
       const token = await AsyncStorage.getItem('token');
       const keys = JSON.parse(token);
       if (token) {
         request.headers[accessToken] = keys.token;
         request.headers.client = keys.client;
         request.headers.uid = keys.uid;
-      } 
+      }
     } catch (error) {}
 
-    if (global.index_request == null) global.index_request = 0;
-    index = global.index_request++;
     return request;
   });
 
   axiosInstance.interceptors.response.use(
-    function(response) {
+    function (response) {
       return response;
     },
-    async error => {
+    async (error) => {
       return Promise.reject(error);
     },
   );
